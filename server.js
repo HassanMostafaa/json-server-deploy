eb3b52ec0b67a1f64e2e523eb66b303099c8d19e
HassanMostafaa <hassan.bmw2251@gmail.com>
2022-04-10 08:30:25 +0200

    ProcessEnv

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 const data = require("./db.js");
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 3001;
 const jsonServer = require("json-server");
 const server = jsonServer.create();
 const router = jsonServer.router(data);
