e8c5e730e4569a8b653f4908f4bca86bb4e79cb2
HassanMostafaa <hassan.bmw2251@gmail.com>
2022-04-10 08:43:31 +0200

    tweaks

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 const data = require("./db.js");
-const port = process.env.PORT || 3001;
+
 const jsonServer = require("json-server");
 const server = jsonServer.create();
 const router = jsonServer.router(data);
@@ -7,4 +7,9 @@ const middlewares = jsonServer.defaults();
 
 server.use(middlewares);
 server.use(router);
-server.listen(port);
+
+var server_port = process.env.YOUR_PORT || process.env.PORT || 8000;
+var server_host = process.env.YOUR_HOST || "0.0.0.0";
+server.listen(server_port, server_host, function () {
+  console.log("Listening on port %d", server_port);
+});
